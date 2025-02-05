62ba853a470064af5b07c676e1a6d35c6fc14afd
AMAN RAI <94359433+amanrai31@users.noreply.github.com>
2025-02-05 10:40:55 +0530

    Update 01string.js

--- 03string/01string.js
+++ 03string/01string.js
@@ -6,3 +6,5 @@ console.log(x==y, "    ", x===y);
 let a = new String("John");
 let b = new String("John");
 console.log(a==b, "    ", a===b); // Comparing two JavaScript objects always returns false.
+
+console.log(x.lenght);  // String length => str.length
