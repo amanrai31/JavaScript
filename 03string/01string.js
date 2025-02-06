7fcb4d51a87e8deb58ba5fd6d4538e555b5ac5de
amanrai31 <aman970rai@gmail.com>
2025-02-06 09:23:07 +0530

    String methods

--- 03string/01string.js
+++ 03string/01string.js
@@ -1,10 +1,41 @@
+// All string methods return a new string. They don't modify the original string. Strings are immutable
+
 let x = "John";
 let y = new String("John");
 console.log(x==y, "    ", x===y);
 
 
-let a = new String("John");
+let a = new String("John");   // string as object
 let b = new String("John");
 console.log(a==b, "    ", a===b); // Comparing two JavaScript objects always returns false.
 
-console.log(x.lenght);  // String length => str.length
+
+// String methods
+
+let str1 = "Jai Shree Ram"
+
+// 1. String length => str.length
+console.log(str1.length);  
+
+// str.charAt(position), str1.at(position), str.charCodeAt(position)
+console.log("charAt =>",str1.charAt(0),"at =>", str1.at(0));              
+console.log("at with -ve index =>", str1.at(-4),"charCodeAt =>", str1.charCodeAt(0));    
+
+
+// str.slice(start, end) || str.substring(start, end) || str.substr(start, length)
+let str2 = "Apple, Banana, Kiwi";
+console.log("7 to 12 =", str2.slice(7,13), ": without end index =",str2.slice(7), ": negative index", str2.slice(-12,-6))
+
+
+// str.toUpperCase(), str.toLowerCase()
+console.log(str1.toUpperCase(), " : ",str1.toLowerCase())
+
+// str.concat(list of parameters to be added/concate)
+let text1 = "Hello";
+let text2 = "Aman";
+let text3 = text1.concat(" ", text2, " ", "Rai");
+console.log(text3)
+
+// str.trim(), str.trimStart(), str.trimEnd()
+let str3 = "     Aman    ";
+console.log("Orignal", str3, " full trim",str3.trim(), "trim start",str3.trimStart(), "trim end", str3.trimEnd())
--- 03string/README.md
+++ 03string/README.md
@@ -14,6 +14,26 @@ let text = "We are the so-called \"Vikings\" from the north.";
 
 **NOTE :** JS strings are primitive values, created from literals, but string also can be defined as objects using new keyword. But string obj can produce unexpected results, so *use string litterals*.
 
-### JS string methods
+### JS string methods (JS also counts position from 0)
 
 **NOTE :** JS string are primitive so immutable. All string method produce a new string without altering the orignal one. 
+
+1. String length => str.length 
+
+
+2. Extracting String Characters
+- str.charAt(position) || **str.at(position)** => at() method is preferred as it allow to use negtive indexed
+- str.charCodeAt(position) => gives the ASCII value of that character
+
+3. Extracting String Parts (includes start index, excludes end index)
+- **slice(start, end)**            [if you dont give end index, it will slice rest of the string from start] *recommended*
+- substring(start, end)            [-ve start/end values will be treated as 0]
+- substr(start, length)            
+
+4. Converting to Upper and Lower Case
+- str.toUpperCase()
+- str.toLowerCase()
+
+5. str.concat(list of parameters to be added)
+
+6. 
