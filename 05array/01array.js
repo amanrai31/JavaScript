5f23369705f751982ce55f95cb204d601a6c67e0
AMAN RAI <94359433+amanrai31@users.noreply.github.com>
2025-02-21 11:18:06 +0530

    Update 01array.js

--- 05array/01array.js
+++ 05array/01array.js
@@ -1,5 +1,23 @@
 // Array
 
 {
-    const carArr = ["M&M", "Tata"];
+    const carArr = ["Mahindra", "BMW", "Tata", "Toyota"];
+    console.log(carArr[1]);
+    //Adding element at last
+    carArr.push("Benz");
+    console.log(Array.isArray(carArr), (carArr instanceof Array));
+}
+
+
+//Nested array
+{
+const myObj = {
+  name: "John",
+  age: 30,
+  cars: [
+    {name:"Ford", models:["Fiesta", "Focus", "Mustang"]},
+    {name:"BMW", models:["320", "X3", "X5"]},
+    {name:"Fiat", models:["500", "Panda"]}
+  ]
+}
 }
