e95edf3b76aae51eb149e795385ba2c13d8a355a
AMAN RAI <94359433+amanrai31@users.noreply.github.com>
2025-06-09 11:34:31 +0530

    Update one.js

--- random/one.js
+++ random/one.js
@@ -1,16 +1,1 @@
-//Hoisting
-
-// x = 10;
-// console.log(x);
-// const x;                // Syntax error => We have to intialize const
-
-// x = 10;
-// console.log(x);
-// let x;                 // ReferenceError: Cannot access 'x' before initialization
-
-// x = 10;
-// console.log(x);
-// var x;                // No issue
-
-// console.log(x);
-// var x = 10;           // JS only hoist declrations, not the initialization (even if it is var) => Will give undefined
+// Random important question will be here
