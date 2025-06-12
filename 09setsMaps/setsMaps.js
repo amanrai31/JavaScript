715ae4b372c27d645f2a22d094b71912918142f2
AMAN RAI <94359433+amanrai31@users.noreply.github.com>
2025-06-12 23:26:53 +0530

    Update setsMaps.js

--- 09setsMaps/setsMaps.js
+++ 09setsMaps/setsMaps.js
@@ -27,6 +27,7 @@ for (const entity of myIterator){
 
 const fruits = new Map();
 
+const fruits = new Map();
 fruits.set("apples", 500);
 fruits.set("bananas", 300);
 fruits.set("oranges", 200);
@@ -35,10 +36,12 @@ console.log(fruits.get("apples"));
 console.log("typeof map :", typeof fruits," || is fruits instance of maps?", fruits instanceof Map);
 
 const mango = {name: 'mango'};
-const kiwi = {name: 'kiwi'};
-fruits.set(mango: 100);
-fruits.set(kiwi: 150);
-
+const kiwi = {name: 'kiwi'};              // object name is kiwi
+const bananas = {name: 'bananas'};
+fruits.set(mango, 100);
+fruits.set(kiwi, 150);
+fruits.set(bananas, 100);
+console.log("kiwi",fruits.has(kiwi))
 
 console.log(typeof "jhon", typeof 3.14, typeof 1234n, typeof true, typeof x, typeof Symbol() )
 console.log("type of null is object",typeof null);  // This is a well-known bug in JS and has historical reasons.
