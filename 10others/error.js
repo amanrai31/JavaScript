7dd595af529f27f6029b7622d4eee2773061cb2a
AMAN RAI <94359433+amanrai31@users.noreply.github.com>
2025-06-16 12:28:09 +0530

    Update error.js

--- 10others/error.js
+++ 10others/error.js
@@ -15,12 +15,12 @@ finally{
 // throw statement
 let x = "2";                              // Change the value of x and see the diff results.
   try {
-    if(x.trim() == "") throw {name: "Empty", message: "Kya kr ra h bhai, kuch to value daal"};
-    if(isNaN(x)) throw "not a number";
+    if(x.trim() == "") throw {name: "Empty", message: "Kya kr ra h bhai, kuch to value daal"}; // thrown err will be an object 
+    if(isNaN(x)) throw "not a number";                                                         // thrown err will be a string
     x = Number(x);
     if(x < 5) throw "too low";
     if(x > 10) throw "too high";
   }
   catch(err) {
-    console.log("Input is ", err);
+    console.log("Input is ", err, "typeof err =>", typeof err);
   }
