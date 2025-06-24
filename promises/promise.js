b3d841d16502bc0c4ba0bef03af269f8bc63018f
AMAN RAI <94359433+amanrai31@users.noreply.github.com>
2025-06-24 23:02:45 +0530

    Update promise.js

--- promises/promise.js
+++ promises/promise.js
@@ -1,33 +1,41 @@
+{
+// Promise
+new Promise ((resolve,reject)=>{
+console.log(1);                            // Body will execute synchronously, it's only resolve which executes asynchronously
+resolve(2)
+})
+.then(result => console.log(result))
+
+console.log(3);
+}
+
+{
+// Promise chaining
 new Promise((resolve,reject)=>{
     resolve(1)                       
 })
 .then(result => result*2)
 .then(result => result*2)
 .then(result => console.log("1st promise result",result))
+}
 
 
 
-// function loadImage(src){                              // Real world promise looks like this
-//     return new Promise((resolve, reject)=>{
-//         const img = new Image();
-//         Image.onload = () => resolve(img);
-//         img.onerror = reject;
-//         img.src = src;
-//     })
-// }
+{
+// Real world promise looks like this    
+function loadImage(src){                              
+    return new Promise((resolve, reject)=>{
+        const img = new Image();
+        Image.onload = () => resolve(img);
+        img.onerror = reject;
+        img.src = src;
+    })
+}
 
-// loadImage(src)
-// .then (image => resizeImage(image))
-// .then (image => addWatermark(image))
-
-
-new Promise ((resolve,reject)=>{
-    console.log(1);                            // Body will execute synchronously, it's only resolve which executes asynchronously
-    resolve(2)
-})
-.then(result => console.log(result))
-
-console.log(3);
+loadImage(src)
+.then (image => resizeImage(image))
+.then (image => addWatermark(image))
+}
 
 {
     Promise.resolve()
@@ -41,6 +49,6 @@ console.log(3);
     
     console.log(5);
 
-    (async () => console.log())();
+    (async () => console.log(6))();
     
 }
