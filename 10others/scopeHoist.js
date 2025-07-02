d5f56092ab953d9ae51788a46b5ee568829401f7
AMAN RAI <94359433+amanrai31@users.noreply.github.com>
2025-07-02 11:37:39 +0530

    Update scopeHoist.js

--- 10others/scopeHoist.js
+++ 10others/scopeHoist.js
@@ -3,31 +3,31 @@
 // Point to remmember => 1st => What is it, is it const, let or var or not decleared at all, 2nd => where it is defined, inside block, function or global scope.
 
 // {
-//   x = 10;     // Not defined that it is var, let or const => will be a global vaiable
+//   x = 10;                                                   // Not defined that it is var, let or const => will be a global vaiable
 // }
-// console.log(x);  // Will give 10;
+// console.log(x);                                             // Will give 10;
 
 // {
-//   var x = 10;     // var does not respect block scope so x will be accessible outside
+//   var x = 10;                                               // var does not respect block scope so x will be accessible outside
 // }
-// console.log(x);  // Will give 10;
+// console.log(x);                                             // Will give 10;
 
 // {
-//   let x = 10;     // let and const do respect block scope
+//   let x = 10;                                               // let and const do respect block scope
 // }
-// console.log(x);   // Will give ReferenceError
+// console.log(x);                                             // Will give ReferenceError
 
 let x = 5;
 {
-  let x = 3;          // this x is valid for this block only. If we do var here => SyntaxError: Identifier 'x' has already been declared
+  let x = 3;                                           // x is valid for this block. If we do var here => SyntaxError: Identifier 'x' has already been declared
 }
-console.log("x", x);
+console.log("x", x);                                            // 5
 
 //////////////////////////////////
 
 let y = 5;
 function myfunc() {
-  y = 3;                    // here y already declared in global scope, so it will re-assign y = 3 
+  y = 3;                                                        // here y already declared in global scope, so it will re-assign y = 3 
 }
 myfunc();
 console.log("y", y);
@@ -36,10 +36,10 @@ console.log("y", y);
 
 let y = 5;
 function myfunc() {
-  x = 3;                    // x will be global 
+  x = 3;                                                          // x will be global 
 }
 myfunc();
-console.log("y =>", y, "||", "x =>" x);  // x will be a global variable, but if we do this in strict mode- will give ReferenceError
+console.log("y =>", y, "||", "x =>" x);                           // x will be a global variable, but if we do this in strict mode- will give ReferenceError
 
 //Hoisting
 
