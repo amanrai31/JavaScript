060d93c7379ccf509dc307d6b5c36ba2d94f47f0
AMAN RAI <94359433+amanrai31@users.noreply.github.com>
2025-07-02 12:40:53 +0530

    Update eventLoop.js

--- eventLoop/eventLoop.js
+++ eventLoop/eventLoop.js
@@ -1,13 +1,1 @@
-Promise.resolve().then (()=> console.log(1));
 
-setTimeout(()=> console.log(2));
-setTimeout(()=>console.log(6),10);
-
-queueMicrotask(()=>
-{
-console.log(3)
-queueMicrotask(()=> console.log(4))
-}
-);
-
-console.log(5);
